bb3ccee3c6909adaa60f1df21be81ea4bae98396
JK <piromaniak@gmail.com>
2021-02-11 20:35:21 +0100

    [general] finished basic functionality

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,21 +4,63 @@ namespace App\Controller;
 
 use phpbrowscap\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 use App\Entity\Link;
 use App\Entity\MetaAccess;
 
 class AppController extends AbstractController
 {
+    private function getRndString($l) {
+        $charset = '0123456789abcdefghijklmnoprstuvxyz';
+        $rndString = '';
+
+        for( $i = 0; $i < $l; $i++ ) {
+            $index = rand(0, strlen($charset) - 1);
+            $rndString .= $charset[$index];
+        }
+
+        return $rndString;
+    }
+
     /**
      * @Route("/", name="app")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $link = new Link();
+
+        $form = $this->createFormBuilder($link)
+            ->add('longLink', TextType::class)
+            ->add('protocol', HiddenType::class, ['empty_data' => 'http://', 'mapped' => false])
+            ->add('submit', SubmitType::class, ['label' => 'Get Short Link!'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $link = $form->getData();
+            $protocol = $form['protocol']->getData();
+            $longLink = $form['longLink']->getData();
+            $link->setLongLink($protocol.$longLink);
+            $link->setShortLink($this->getRndString(6));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($link);
+            $em->flush();
+
+            return $this->render('app/shortLink.html.twig', [
+                'shortLink' => $link->getShortLink(),
+            ]);
+        }
+
         return $this->render('app/index.html.twig', [
-            'controller_name' => 'lol',
+            'form' => $form->createView(),
         ]);
     }
 
